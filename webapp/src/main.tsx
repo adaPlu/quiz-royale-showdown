f498bc95969767bb56f32883474d89eb7de9a2a9
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-08-23 20:28:30 -0700

    Install optional client telemetry at web startup

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./AppNext";
+import { installClientTelemetry } from "./telemetry";
 import "./styles.css";
+
+installClientTelemetry();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
