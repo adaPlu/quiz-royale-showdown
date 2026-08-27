52d0cdf2293fc3d28095d1a41696bc28acb32908
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-08-26 20:42:28 -0700

    Bump Android alpha to 1.5 code 5

--- android-quiz-royale-showdown/app/build.gradle.kts
+++ android-quiz-royale-showdown/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.rork.quizroyaleshowdown"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.5"
         buildConfigField("String", "EXPO_PUBLIC_RAILWAY_API_URL", railwayApiUrl.toBuildConfigLiteral())
         buildConfigField("String", "EXPO_PUBLIC_RORK_FUNCTIONS_URL", rorkFunctionsUrl.toBuildConfigLiteral())
     }
