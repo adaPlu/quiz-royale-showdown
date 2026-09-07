8599cbfdd26c587781ec4609fcfd66372d816aca
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-09-06 18:57:14 -0700

    chore(android): adopt compileSdk 37 and built-in Kotlin

--- android-quiz-royale-showdown/app/build.gradle.kts
+++ android-quiz-royale-showdown/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
 }
@@ -61,7 +60,7 @@ requireReleaseValue("EXPO_PUBLIC_RORK_FUNCTIONS_URL", rorkFunctionsUrl)
 
 android {
     namespace = "com.rork.quizroyaleshowdown"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.rork.quizroyaleshowdown"
