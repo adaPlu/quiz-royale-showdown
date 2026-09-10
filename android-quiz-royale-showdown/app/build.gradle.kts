bf580d8187a8d1af9ac7d47dcf65b7ddf8c80a00
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-09-10 01:38:40 -0700

    build: bump Android v2.0 versionCode to 1787428690

--- android-quiz-royale-showdown/app/build.gradle.kts
+++ android-quiz-royale-showdown/app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
         applicationId = "com.rork.quizroyaleshowdown"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1787428689
+        versionCode = 1787428690
         versionName = "2.0"
         buildConfigField("String", "EXPO_PUBLIC_RAILWAY_API_URL", railwayApiUrl.toBuildConfigLiteral())
         buildConfigField("String", "EXPO_PUBLIC_RORK_FUNCTIONS_URL", rorkFunctionsUrl.toBuildConfigLiteral())
